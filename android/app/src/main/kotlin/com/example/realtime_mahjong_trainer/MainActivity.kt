9088e7709865a44892be2cc78595d76458f8edea
heyzec <61238538+heyzec@users.noreply.github.com>
2024-01-10 13:05:14 +0800

    feat: initialise flutter project

---
+++ android/app/src/main/kotlin/com/example/realtime_mahjong_trainer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.realtime_mahjong_trainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flake.nix
+++ flake.nix
@@ -1,38 +1,55 @@
 {
-  description = "Template for a direnv shell, with Python";
+description = "Flutter 3.13.x";
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
+      };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ buildToolsVersion ];
+        platformVersions = [ "33" "34" "32" ];
+        # abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = [
+            flutter
+            androidSdk
+            jdk17
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }:
-  let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        (let
-          python-packages = ps: with ps; [
-            (buildPythonPackage rec {
-            pname = "mahjong";
-              version = "1.2.1";
-              src = fetchPypi {
-                inherit pname version;
-                sha256 = "r77y9f6mVrc+rLY5YXbR6AzQYSPCTb6iffhxAOhQIJc=";
-              };
-              doCheck = false;
-              propagatedBuildInputs = [
-                # # Specify dependencies
-                # pkgs.python3Packages.numpy
+            (let
+              python-packages = ps: with ps; [
+                (buildPythonPackage rec {
+                pname = "mahjong";
+                  version = "1.2.1";
+                  src = fetchPypi {
+                    inherit pname version;
+                    sha256 = "r77y9f6mVrc+rLY5YXbR6AzQYSPCTb6iffhxAOhQIJc=";
+                  };
+                  doCheck = false;
+                  propagatedBuildInputs = [
+                    # # Specify dependencies
+                    # pkgs.python3Packages.numpy
+                  ];
+                })
               ];
-            })
+            in python3.withPackages python-packages)
           ];
-        in python3.withPackages python-packages)
-      ];
-    };
-  };
+        };
+    });
 }
 
